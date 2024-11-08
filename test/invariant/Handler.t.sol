24780fdc247209c5d3a7cca646e18aa893a93a2a
ybtuti <samuelmsuu@gmail.com>
2024-11-08 12:56:55 +0300

    Implement liquidity provider deposit logic in Handler contract

--- test/invariant/Handler.t.sol
+++ test/invariant/Handler.t.sol
@@ -11,6 +11,18 @@ contract Handler is Test {
     ERC20Mock weth;
     ERC20Mock poolToken;
 
+    address liquidityprovider = makeAddr("lp");
+
+    // Ghost Variables
+    int256 startingY;
+    int256 startingX;
+
+    int256 expectedDeltaY;
+    int256 expectedDeltaX;
+
+    int256 actualDeltaX;
+    int256 actualDeltaY;
+
     constructor(TSwapPool _pool) {
         pool = _pool;
         weth = ERC20Mock(_pool.getWeth());
@@ -23,5 +35,27 @@ contract Handler is Test {
         // Lets make sure its a reasonable Amount
         // avoid wierd overflow errors
         wethAmount = bound(wethAmount, 0, type(uint64).max);
+
+        startingY = int256(weth.balanceOf(address(this)));
+        startingX = int256(poolToken.balanceOf(address(this)));
+        expectedDeltaY = int256(wethAmount);
+        expectedDeltaX = int256(pool.getPoolTokensToDepositBasedOnWeth(wethAmount));
+
+        // deposit
+        vm.startPrank(liquidityprovider);
+        weth.mint(liquidityprovider, wethAmount);
+        poolToken.mint(liquidityprovider, uint256(expectedDeltaX));
+        weth.approve(address(pool), type(uint256).max);
+        poolToken.approve(address(pool), type(uint256).max);
+
+        pool.deposit(wethAmount, 0, uint256(expectedDeltaX), uint64(block.timestamp));
+        vm.stopPrank();
+
+        // actual
+        uint256 endingY = weth.balanceOf(address(this));
+        uint256 endingX = poolToken.balanceOf(address(this));
+
+        actualDeltaY = int256(endingY) - int256(startingY);
+        actualDeltaX = int256(endingX) - int256(startingX);
     }
 }
